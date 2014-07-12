46babd80b80c81f6765e0667c2cbc4bc083154c4
Max Vasileusky <vasileusky@gmail.com>
2014-07-12 13:26:00 +0300

    converter fix

--- src/main/java/by/mvas/sprgr/formatter/QuoteTagConverter.java
+++ src/main/java/by/mvas/sprgr/formatter/QuoteTagConverter.java
@@ -2,11 +2,13 @@ package by.mvas.sprgr.formatter;
 
 import by.mvas.sprgr.model.QuoteTag;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author mvas
  */
+@Component
 public class QuoteTagConverter implements Converter<String, QuoteTag> {
 
     @Override
